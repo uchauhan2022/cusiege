46d34e57e5418378f7544b203676dbbda106b897
Prakhar Singh <prakharsingh13@gmail.com>
2019-11-01 20:06:10 +0530

    fix

--- play.php
+++ play.php
@@ -120,4 +120,11 @@ $(document).ready(function(){
 	$.get('pre.php',function(data){
 		  $('#questions_div').html(data);
 		  });
+	setTimeout(function(){
+		for(var i=0;i<10;i++){
+			$.post('pre.php?marks',{qid:i},function(data){
+				$('#marks_div:'.i).html(data);
+			});
+		}
+	},3000)
 });</script>
