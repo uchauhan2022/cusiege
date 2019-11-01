5c747f8b3592480fe82c2d0b4134515059b288ce
Prakhar Singh <prakharsingh13@gmail.com>
2019-11-01 17:32:43 +0530

    fix

--- rule1.php
+++ rule1.php
@@ -8,6 +8,10 @@ if((!isset($_SESSION['username'])))
 
 <html>
    <head>
+	    <link rel="stylesheet" href="Assets/css/style.css">
+     
+     <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet">
+     
  <link rel='icon' href='icon/favicon.ico' type='image/x-icon'>
    <title>Rules</title>
    <link rel='stylesheet' href='Assets/css/1.css'>
