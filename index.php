c675984ae79845b7c9b56be07760351deedca46b
Prakhar Singh <prakharsingh13@gmail.com>
2019-11-05 21:45:36 +0530

    fix

--- index.php
+++ index.php
@@ -23,9 +23,7 @@ header("location:Assets/php/redirect.php")
                echo "<br><br> Wrong Credentials.";
                ?>
                 <h1><b>LOGIN</b></h1><input type="Textbox" placeholder="Roll Number" id="textbox" name="username"><br><br>
-            <input type="Password" placeholder="Password" id="textbox" name="password"><br><br>
-				   <div class="g-recaptcha" data-sitekey="6LdgL8EUAAAAAPvAIKd8EgHETEQwtZfCURKZaz-F"></div><br>
-				   <input type="submit" id="button" value="Login"><br><br>Need an account? <a href='signup.php'>Sign Up</a> </center>
+            <input type="Password" placeholder="Password" id="textbox" name="password"><br><br><input type="submit" id="button" value="Login"><br><br>Need an account? <a href='signup.php'>Sign Up</a> </center>
         </form></div></div>
 </body>
 </html>
