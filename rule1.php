a8d9aee36d1ba9fcaae55135fa37dc7e539f67f9
Prakhar Singh <prakharsingh13@gmail.com>
2020-01-19 21:09:10 +0530

    fix

--- rule1.php
+++ rule1.php
@@ -36,8 +36,42 @@ if((!isset($_SESSION['username'])))
 </ul></div><br><br>
 		  <?php
 		  $now=strtotime('now');
-		  if(!$_SESSION['is_admin'] && $now<strtotime('22 January 2019 22:00:00'))
-		  echo '<div style="color:white; font-size:23px;">Event will start at 9:30 PM.</div>';
+		  if(!$_SESSION['is_admin'] && $now<strtotime('22 January 2020 22:00:00'))
+		  echo '<div style="color:white; font-size:23px;">Event will start in <div class="sub_navbartimer"><div id="clockdiv" style="
+    zoom: 0.4;
+		  -moz-transform: scale(0.4);"><span id="tl">Time Left :</span>
+	<div><span class="hours" id="hours">00</span><div class="smalltext">Hours</div></div>
+	<div><span class="minutes" id="minutes">00</span><div class="smalltext">Minutes</div></div>
+	<div><span class="seconds" id="seconds">00</span><div class="smalltext">Seconds</div></div>
+	</div></div><form method="post" action="finish.php" name="finalSubmit"></form>.</div>
+	<script type = "text/javascript">
+
+	var deadline = <?php
+if ( $_SESSION[ \'lev\' ] == 1 )
+	echo   strtotime( \'22 January 2020 22:00:00\' ) - strtotime( \'now\' );
+elseif ( $_SESSION[ \'lev\' ] == 2 )
+	echo  strtotime( \'24 January 2020 22:00:00\' ) - strtotime( \'now\' );
+		elseif ( $_SESSION[ \'lev\' ] == 3 )
+	echo  strtotime( \'26 January 2020 22:00:00\' ) - strtotime( \'now\' );
+?>;
+var t = deadline ;
+var downloadTimer = setInterval( function () {
+	t--;
+	var seconds = Math . floor( ( t ) % 60 );
+	var minutes = Math . floor( ( t / 60 ) % 60 );
+	var hours = Math . floor( ( t / ( 60 * 60 ) ) % 24 );
+	document . getElementById( "hours" ) . textContent = ( \'0\' + hours ) . slice( -2 );
+	document . getElementById( "minutes" ) . textContent = ( \'0\' + minutes ) . slice( -2 );
+	document . getElementById( "seconds" ) . textContent = ( \'0\' + seconds ) . slice( -2 );
+	if ( t <= 0 ) {
+		clearInterval( downloadTimer );
+		document . finalSubmit . submit();
+	}
+}, 1000 );
+
+
+
+</script>';
 		  else 
 			 echo'
 		<form action="lev1.php" target="_parent" method="post">
